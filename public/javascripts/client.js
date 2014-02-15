c01ec5838cf6eb66bb7b9ec9dcbc81bc864b1c95
Chunmun <xilasirc@gmail.com>
2014-02-15 17:55:34 +0800

    Add player

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -10,10 +10,33 @@ var Client = function () {
   console.log("Starting Game")
 
   game.loadMap();
+
+  // ===== Add Player Test code here
+  var p = {
+    id: '1234',
+    name: 'test',
+    HP: 3,
+    direction: 0, // 0 - Up, 1 - Right, 2 - Down, 3 - Left
+    x: 0,
+    y: 0
+  };
+
+  game.addPlayer(p);
+  // =====
+
   game.start();
   console.log("Loaded Game")
 };
 
+GameConfig = {
+  tileSize: 20,
+  padding: 2,
+  playerSize: 18
+};
+
+var xyToPix = function(pt) {
+  return {x:pt.x*(GameConfig.tileSize+GameConfig.padding),y:pt.y*(GameConfig.tileSize+GameConfig.padding)}
+}
 
 var Game = function (stage) {
   this.players = [];
@@ -29,8 +52,6 @@ var Game = function (stage) {
   // Map
   this.rows = 40;
   this.cols = 30;
-  this.tileSize = 20;
-  this.padding = 2;
 
   createjs.Ticker.addEventListener('tick', _.bind(this.handleTick, this));
 }
@@ -39,12 +60,12 @@ Game.prototype.loadMap = function () {
   for (var i=0;i<this.rows;i++) {
     for (var j=0;j<this.cols;j++) {
       var tile = new createjs.Shape();
-      tile.graphics.beginFill("#ff0000").drawRect(0, 0, this.tileSize, this.tileSize);
+      tile.graphics.beginFill("#ff0000").drawRect(0, 0, GameConfig.tileSize, GameConfig.tileSize);
       var computedAlpha = Math.abs(i-this.rows)/this.rows;
       tile.alpha = computedAlpha;
-
-      tile.x = i * (this.tileSize + this.padding);
-      tile.y = j * (this.tileSize + this.padding);
+      var pt = xyToPix({x:j,y:i});
+      tile.x = pt.x;
+      tile.y = pt.y;
       this.stage.addChild(tile);
     }
   }
@@ -117,25 +138,29 @@ Game.prototype.removePlayer = function (player) {
 }
 
 var Player = function(data) {
+  this.data = data;
   this.id = data.id;
   this.name = data.name;
 
   // Easeljs stuff
   this.view = new createjs.Shape();
-  this.view.graphics.beginFill("#ff0000").drawRect(0, 0, 100, 100);
+  var leftPadding = Math.abs(GameConfig.tileSize - GameConfig.playerSize) / 2;
+  this.view.graphics.beginFill("#00ff00").drawRect(leftPadding, leftPadding, GameConfig.playerSize, GameConfig.playerSize);
 
-  this.view.x = data.x;
-  this.view.y = data.y;
+  var xy = xyToPix(data);
+  this.view.x = xy.x;
+  this.view.y = xy.y
 
   this.x = data.x;
   this.y = data.y;
 
-  this.view.alpha = 0;
+  this.view.alpha = 1;
 }
 
 Player.prototype.tick = function () {
-  this.view.x = this.x;
-  this.view.y = this.y;
+  var xy = xyToPix({x:this.x, y:this.y});
+  this.view.x = xy.x;
+  this.view.y = xy.y
 }
 
 Player.prototype.die = function () {
